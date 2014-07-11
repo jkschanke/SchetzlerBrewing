ef772ca49a70f559c331b2c9614eb4755d8e8b56
jkschanke <Coolermaster11>
2014-07-11 16:43:02 -0500

    fix css

--- CurrentStock.Web/CurrentStock.Web/App_Start/BundleConfig.cs
+++ CurrentStock.Web/CurrentStock.Web/App_Start/BundleConfig.cs
@@ -14,11 +14,14 @@ namespace App.CurrentStock.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/content/css/app").Include("~/content/app.css"));
+            bundles.Add(new StyleBundle("~/content/css/app")
+                .Include("~/content/app.css"));
 
-            bundles.Add(new ScriptBundle("~/js/jquery").Include("~/scripts/vendor/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/js/jquery")
+                .Include("~/scripts/vendor/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/js/app").Include(
+            bundles.Add(new ScriptBundle("~/js/app")
+                .Include(
                 "~/scripts/vendor/angular-ui-router.js",
                 "~/scripts/filters.js",
                 "~/scripts/services.js",
