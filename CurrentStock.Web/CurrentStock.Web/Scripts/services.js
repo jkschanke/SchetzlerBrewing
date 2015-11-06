8c67b165d311cc0768d29dc68aee1722c8d8a029
jkschanke <Coolermaster11>
2015-11-05 21:49:31 -0600

    update beers

--- CurrentStock.Web/CurrentStock.Web/Scripts/services.js
+++ CurrentStock.Web/CurrentStock.Web/Scripts/services.js
@@ -7,8 +7,7 @@ angular.module('app.services', [])
         var instance = {};
 
         instance.bottles = [
-            { name: 'Orange Chocolate Stout', style: 'Stout', type: 'All-Grain', brewDate: '05/06/2015', originalGravity: 1.045, finalGravity: 1.010, description: 'Stout brewed with orange peel and cocoa powder for a chocolatey finish. This kind of sucks, try at your own risk', icon: '/content/images/peatsmokedporter.jpg', capCodes: ['OC'] },
-            { name: 'Speckled Heifer', style: 'American Pale Ale/Cream Ale', type: 'Partial Mash', brewDate: '07/27/2015', originalGravity: 1.044, finalGravity: 1.010, description: 'A tranquil time-out in a pint glass, a pastoral idyll, a sociable session ale dairyland-style: hey, sometimes a beer is just a beer. Have one.', icon: '/content/images/speckled heifer.jpg', capCodes: ['SH'] },
+            {  name: 'Speckled Heifer', style: 'American Pale Ale/Cream Ale', type: 'Partial Mash', brewDate: '07/27/2015', originalGravity: 1.044, finalGravity: 1.010, description: 'A tranquil time-out in a pint glass, a pastoral idyll, a sociable session ale dairyland-style: hey, sometimes a beer is just a beer. Have one.', icon: '/content/images/speckled heifer.jpg', capCodes: ['SH'] },
             { name: 'Gaarden Hoe Witbier', style: 'Trappist/Belgian Ale, Wheat Beers', type: 'Extract', brewDate: '07/14/2015', originalGravity: 1.049, finalGravity: 1.010, description: 'The perfect pint after a hard day’s work, Gaarden Hoe is an ode to the historic and refreshing Belgian wit. Classic wheat is expertly tilled into a traditional pilsner malt bill and modest, grounded hop profile. Coriander and bitter orange peel offer slightly tart and citrusy undertones to quench even the most parched palate.', icon: '/content/images/garden hoe.jpg', capCodes: ['GH'] }
         ]
 
@@ -21,7 +20,7 @@ angular.module('app.services', [])
         }
 
         instance.rightTap = {
-            name: 'Honey Kolsch', style: 'German Ale', type: 'Extract', brewDate: '06/29/2015', originalGravity: 1.048, finalGravity: 1.008, description: 'A variation on our extremely popular Kölsch recipe with the addition of honey and a new yeast strain for a lighter body and flavor. Like our original Kölsch kit, this recipe features a pale color, light body, and smooth mouthfeel. Honey adds subtle floral character and lightens the body by fermenting to dryness. Really nice as a summer brew for barbecues and picnics, or to wean your friends off watery mass-produced lagers.', icon: '/content/images/kolsch.jpg'
+            name: 'Dark Cherry Stout', style: 'Spice or Fruit Beer, Stout/Porter', type: 'Extract', brewDate: '09/09/2015', originalGravity: 1.052, finalGravity: 1.018, description: 'A classic dry stout, black in color, with the sharp bite of roasted barley and laced with cherry extract. Not overpowering like many fruit beers, this is a more restrained brew.', icon: '/content/images/darkcherrystout.jpg'
         }
 
         return instance;
